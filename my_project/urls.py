2648bdc3b07610dc6e4213985e662872abe9ebc4
Keith O'Donoghue <amky2syl@students.codeinstitute.net>
2024-09-05 13:59:56 +0000

    Migrate

--- my_project/urls.py
+++ my_project/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from home import views as home_views
+
 
 urlpatterns = [
+    path('', home_views.HomePage, name='home'),
     path('admin/', admin.site.urls),
-    path('', include('home.urls'), name='home_urls'),
-]
+    ]
