37c5eadb3e06a2cdbea6df2c803b57a28b94c9d4
Keith O'Donoghue <amky2syl@students.codeinstitute.net>
2024-09-10 23:14:41 +0000

    Add code for create new account form

--- bookings/views.py
+++ bookings/views.py
@@ -5,5 +5,9 @@ from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
 class CreateAccount(TemplateView):
-    form = UserCreationForm()
     template_name = 'create-account.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = UserCreationForm()
+        return context
