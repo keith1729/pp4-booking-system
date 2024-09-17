5be0218fe37e60b28f22d396b43ff948fd752dfe
Keith O'Donoghue <amky2syl@students.codeinstitute.net>
2024-09-17 14:39:54 +0000

    Create booking form template

--- booking/models.py
+++ booking/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
+
 
 # Create your models here.
 
 class Booking(models.Model):
   user = models.ForeignKey(User, on_delete=models.CASCADE)
+  email = models.EmailField()
   date = models.DateField()
   time = models.TimeField()
   number_of_players = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(4)])
@@ -12,3 +15,5 @@ class Booking(models.Model):
 
   def __str__(self):
         return f"Booking by {self.user.username} on {self.date} at {self.time} for {self.number_of_players} players!"
+
+
