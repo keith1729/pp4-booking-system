ec4b65b7d4b248767424202f4e0dfa618fed8211
Keith O'Donoghue <amky2syl@students.codeinstitute.net>
2024-09-18 14:43:25 +0000

    Add registration form

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import users_home, RegistrationView
+from .views import RegistrationView
 
 # URL config
 urlpatterns = [
-    path('', users_home.as_view(), name='users_home'),
+    # path('', users_home.as_view(), name='users_home'),
     path('registration/', RegistrationView.as_view(), name='users_registration'),
 ]
